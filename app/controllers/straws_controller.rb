b13acab44a2fc1faeb9786e249e581815524ef58
brentmartin <bmart35@gmail.com>
2017-01-03 23:59:39 -0600

    Added notes for organization

--- app/controllers/straws_controller.rb
+++ app/controllers/straws_controller.rb
@@ -2,11 +2,14 @@ class StrawsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
+# basic parsing
     straw_usernames = params[:text].split(' ')
     short_straw_username = straw_usernames.sample
     host_username = params[:user_name]
 
+# advanced parsing
     straw_task_description = params[:text].scan(/\"(.+)\"/).flatten.join
+# message creation
     straw_host_message = "A straw drawing was created by @#{host_username} "
     straw_participants = "created a straw drawing for #{straw_usernames.to_sentence}."
     straw_selection_message = "#{short_straw_username} has drawn the short straw!"

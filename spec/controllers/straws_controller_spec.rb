598c6029415c62c8782b915246bb1ad73034a319
brentmartin <bmart35@gmail.com>
2017-01-05 17:53:24 -0600

    Switch "text" to "attachments" including nested json

--- spec/controllers/straws_controller_spec.rb
+++ spec/controllers/straws_controller_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe StrawsController, :type => :controller do
       post :create, req
 
       json_response = JSON.parse(response.body)
-      expect(json_response["text"]).to include("@jess")
+      expect(json_response["attachments"][0]["fields"][0]["value"]).to include("@jess")
     end
 
-    it "chooses a user name from the list of user names" do
+    it "chooses a short straw from the list of user names" do
       req = slack_request
       req[:text] = "@elizabeth @brent"
 
       post :create, req
 
-      straws_response = JSON.parse(response.body)["text"]
+      straws_response = JSON.parse(response.body)["attachments"][1]["text"]
 
       expect(
         straws_response.include?("@elizabeth") || straws_response.include?("@brent")
@@ -33,7 +33,7 @@ RSpec.describe StrawsController, :type => :controller do
       post :create, req
 
       json_response = JSON.parse(response.body)
-      expect(json_response["text"]).to include("@jess")
+      expect(json_response["attachments"][0]["pretext"]).to include("@jess")
     end
 
     it "lists all the usernames involved in the straw poll" do
@@ -43,22 +43,22 @@ RSpec.describe StrawsController, :type => :controller do
       post :create, req
 
       json_response = JSON.parse(response.body)
-      expect(json_response["text"]).to include("@elizabeth and @brent")
+      expect(json_response["attachments"][0]["fields"][0]["value"]).to include("@elizabeth\n@brent")
     end
   end
 end
 
 def slack_request
   {
-    "token"=>"5oqrzVhONee6k5RpfdH1XgWj", 
-    "team_id"=>"T3JRVTPCJ", 
-    "team_domain"=>"drawstraws", 
-    "channel_id"=>"C3J530CNL", 
-    "channel_name"=>"general", 
-    "user_id"=>"U3J534MDW", 
-    "user_name"=>"jess", 
-    "command"=>"/straws", 
-    "text"=>"some text", 
+    "token"=>"5oqrzVhONee6k5RpfdH1XgWj",
+    "team_id"=>"T3JRVTPCJ",
+    "team_domain"=>"drawstraws",
+    "channel_id"=>"C3J530CNL",
+    "channel_name"=>"general",
+    "user_id"=>"U3J534MDW",
+    "user_name"=>"jess",
+    "command"=>"/straws",
+    "text"=>"some text",
     "response_url"=>"https://hooks.slack.com/commands/T3JRVTPCJ/120179635216/xNvGpRgvr9aG6pna4mUAnu1U"
   }
 end
